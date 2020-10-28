3f02a3240a408727084f1a9c5c5488ee2012d2ac
stupidkitty <adx.informer@protonmail.com>
2020-10-28 11:11:00 +0300

    Use const instead let

--- src/components/modals/ModalControlBar.vue
+++ src/components/modals/ModalControlBar.vue
@@ -4,25 +4,25 @@ export default {
   props: {
     sticky: {
       type: Boolean,
-      default: true,
-    },
+      default: true
+    }
   },
-  mounted() {
+  mounted () {
     if (this.sticky) {
-      let overlay = this.$el.closest('.v--modal-overlay');
+      const overlay = this.$el.closest('.v--modal-overlay')
 
-      if (null !== overlay) {
+      if (overlay !== null) {
         overlay.addEventListener('scroll', () => {
-          let ovRect = overlay.getBoundingClientRect();
-          let elRect = this.$el.getBoundingClientRect();
-          let offsetY = elRect.top - ovRect.top;
+          const ovRect = overlay.getBoundingClientRect()
+          const elRect = this.$el.getBoundingClientRect()
+          const offsetY = elRect.top - ovRect.top
 
           if (overlay.scrollTop > offsetY) {
-            this.$el.classList.add('-sticky');
+            this.$el.classList.add('-sticky')
           } else {
-            this.$el.classList.remove('-sticky');
+            this.$el.classList.remove('-sticky')
           }
-        });
+        })
       }
     }
   }
