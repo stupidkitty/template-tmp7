658a24c573d4c7d62e34e46dbcf19c5e1fa0e708
stupidkitty <adx.informer@protonmail.com>
2020-11-06 12:12:38 +0300

    Clean code and fix types

--- src/js/helpers/Humanizer.js
+++ src/js/helpers/Humanizer.js
@@ -1,56 +1,55 @@
-import TimeAgo from 'javascript-time-ago';
-import ru from 'javascript-time-ago/locale/ru';
+import TimeAgo from 'javascript-time-ago'
+import ru from 'javascript-time-ago/locale/ru'
 
-TimeAgo.addLocale(ru);
-const timeAgo = new TimeAgo('ru-RU');
+TimeAgo.addLocale(ru)
+const timeAgo = new TimeAgo('ru-RU')
 
-class Humanizer
-{
-  seconds2time(seconds) {
-    let h = Math.floor(seconds / 3600);
-    let m = Math.floor((seconds % 3600) / 60);
-    let s = seconds % 60;
+class Humanizer {
+  seconds2time (seconds) {
+    const h = Math.floor(seconds / 3600)
+    const m = Math.floor((seconds % 3600) / 60)
+    const s = seconds % 60
 
     return [
       h,
       m > 9 ? m : (h ? '0' + m : m || '0'),
-      s > 9 ? s : '0' + s,
-    ].filter(a => a).join(':');
+      s > 9 ? s : '0' + s
+    ].filter(a => a).join(':')
   }
 
-  compactNumber(value) {
-    let newValue = value;
+  compactNumber (value) {
+    let newValue = value
 
     if (value >= 1000) {
-      let suffixes = ['', 'k', 'm', 'b', 't'];
-      let suffixNum = Math.floor(('' + value).length / 3);
-      let shortValue = '';
+      const suffixes = ['', 'k', 'm', 'b', 't']
+      const suffixNum = Math.floor(('' + value).length / 3)
+      let shortValue = 0
 
       for (let precision = 2; precision >= 1; precision--) {
-        shortValue = parseFloat((suffixNum != 0 ? (value / Math.pow(1000,suffixNum)) : value).toPrecision(precision));
+        shortValue = parseFloat((suffixNum !== 0 ? (value / Math.pow(1000, suffixNum)) : value).toPrecision(precision))
 
-        let dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g,'');
+        const dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g, '')
 
         if (dotLessShortValue.length <= 2) {
-          break;
+          break
         }
       }
 
       if (shortValue % 1 !== 0) {
-        shortValue = shortValue.toFixed(1);
+        shortValue = shortValue.toFixed(1)
       }
 
-      newValue = shortValue + suffixes[suffixNum];
+      newValue = shortValue + suffixes[suffixNum]
     }
 
-    return newValue;
+    return newValue
   }
 
-  timeAgo(publishedAt) {
-    return timeAgo.format(new Date(publishedAt));
+  timeAgo (publishedAt) {
+    return timeAgo.format(new Date(publishedAt))
   }
 }
 
-const hm = new Humanizer();
+const hm = new Humanizer()
 
-export default hm;
+export default hm
